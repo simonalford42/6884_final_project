3b1a959274fc145fd1977e5c057205a255ba8fc7
Simon <salford@mit.edu>
2020-12-02 16:08:31 -0500

    made train/test split method

--- seq2seq.py
+++ seq2seq.py
@@ -15,30 +15,24 @@ plt.switch_backend('agg')
 import matplotlib.ticker as ticker
 import numpy as np
 
+# use full data for determining input/output language, in case splits somehow change it
+full_data = "scan/SCAN-master/tasks.txt"
+input_lang, output_lang, full_pairs = prepareData('scan_in', 'scan_out', full_data, False)
+# used for train as well as test splits
+# I add one for the <EOS> tag. Not sure if necessary but can't hurt.
+MAX_LENGTH = max(len(pair[i].split(' ')) for pair in full_pairs) + 1
 
-
-data_file = "scan/SCAN-master/simple_split/tasks_test_simple.txt"
-
-input_lang, output_lang, pairs = prepareData('scan_in', 'scan_out', data_file, False)
+split_path = 'scan/SCAN-master/simple_split/tasks_test_simple.txt'
+_, _, pairs = prepareData('scan_in', 'scan_out', data_file, False)
 print('Sample data pair: {}'.format(random.choice(pairs)))
 print('Dataset size: {}'.format(len(pairs)))
 
-INPUT_SIZE = input_lang.n_words
-HIDDEN_SIZE = 200
-OUTPUT_SIZE = output_lang.n_words
-MAX_LENGTH = 10
-
-# set to -1 if no clipping desired
-gradient_clip = 5
-
-teacher_forcing_ratio = 0.5
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print('Torch device: {}'.format(device))
 
 """
 Helpers
 """
-
 
 def showPlot(points):
     plt.figure()
@@ -66,15 +60,15 @@ def indexesFromSentence(lang, sentence):
     return [lang.word2index[word] for word in sentence.split(' ')]
 
 
-def tensorFromSentence(lang, sentence):
+def tensorFromSentence(lang, sentence, device):
     indexes = indexesFromSentence(lang, sentence)
     indexes.append(EOS_token)
     return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
 
 
-def tensorsFromPair(input_lang, output_lang, pair):
-    input_tensor = tensorFromSentence(input_lang, pair[0])
-    target_tensor = tensorFromSentence(output_lang, pair[1])
+def tensorsFromPair(input_lang, output_lang, pair, device):
+    input_tensor = tensorFromSentence(input_lang, pair[0], device)
+    target_tensor = tensorFromSentence(output_lang, pair[1], device)
     return (input_tensor, target_tensor)
 
 
@@ -85,7 +79,7 @@ Model Architecture
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, device):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
@@ -103,7 +97,7 @@ class EncoderRNN(nn.Module):
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
+    def __init__(self, hidden_size, output_size, device):
         super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
@@ -165,8 +159,11 @@ Training
 """
 
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer,
-        decoder_optimizer, criterion, max_length=MAX_LENGTH,
-        gradient_clip=5):
+        decoder_optimizer, criterion, device):
+
+    gradient_clip = 5
+    teacher_forcing_ratio = 0.5
+
     encoder_hidden = encoder.initHidden()
 
     encoder_optimizer.zero_grad()
@@ -175,7 +172,7 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer,
     input_length = input_tensor.size(0)
     target_length = target_tensor.size(0)
 
-    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+    encoder_outputs = torch.zeros(MAX_LENGTH, encoder.hidden_size, device=device)
 
     loss = 0
 
@@ -221,7 +218,7 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer,
 
 
 
-def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100,
+def trainIters(encoder, decoder, n_iters, device, print_every=1000, plot_every=100,
         learning_rate=0.001):
     print('Starting training')
     start = time.time()
@@ -231,7 +228,7 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100,
 
     encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
     decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
-    training_pairs = [tensorsFromPair(input_lang, output_lang, random.choice(pairs))
+    training_pairs = [tensorsFromPair(input_lang, output_lang, random.choice(pairs), device)
                       for i in range(n_iters)]
     criterion = nn.NLLLoss()
 
@@ -244,7 +241,8 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100,
         target_tensor = training_pair[1]
 
         loss = train(input_tensor, target_tensor, encoder,
-                     decoder, encoder_optimizer, decoder_optimizer, criterion)
+                     decoder, encoder_optimizer, decoder_optimizer, criterion,
+                     device)
         print_loss_total += loss
         plot_loss_total += loss
 
@@ -261,7 +259,8 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100,
             plot_losses.append(plot_loss_avg)
             plot_loss_total = 0
 
-    showPlot(plot_losses)
+    # showPlot(plot_losses)
+    return plot_losses
 
 
 """
@@ -269,13 +268,13 @@ Evaluation
 """
 
 
-def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
+def evaluate(encoder, decoder, sentence, device):
     with torch.no_grad():
         input_tensor = tensorFromSentence(input_lang, sentence)
         input_length = input_tensor.size()[0]
         encoder_hidden = encoder.initHidden()
 
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+        encoder_outputs = torch.zeros(MAX_LENGTH, encoder.hidden_size, device=device)
 
         for ei in range(input_length):
             encoder_output, encoder_hidden = encoder(input_tensor[ei],
@@ -288,7 +287,7 @@ def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
 
         decoded_words = []
 
-        for di in range(max_length):
+        for di in range(MAX_LENGTH):
             decoder_output, decoder_hidden = decoder(
                 decoder_input, decoder_hidden)
             topv, topi = decoder_output.topk(1)
@@ -305,32 +304,36 @@ def evaluate(encoder, decoder, sentence, max_length=MAX_LENGTH):
         return decoded_words
 
 
-def evaluateTestSet(encoder, decoder, pairs):
+def evaluateTestSet(encoder, decoder, pairs, device):
     encoder.eval()
     decoder.eval()
 
     with torch.no_grad():
-        print('Evaluating {} test examples'.format(len(pairs)))
         hits = 0
         for pair in pairs:
-            output_words = evaluate(encoder, decoder, pair[0])
+            output_words = evaluate(encoder, decoder, pair[0], device)
             output_sentence = ' '.join(output_words)
             if output_words[-1] == '<EOS>':
                 output_sentence = ' '.join(output_words[:-1])
                 if pair[1] == output_sentence:
                     hits += 1
+            else:
+                assert len(output_words) == MAX_LENGTH, str.format(
+                        'unexpected length: {} but max is {}',
+                        len(output_words), MAX_LENGTH)
 
         print('Evaluation accuracy: {}/{} = {:.2f}%'.format(hits, len(pairs),
             hits/len(pairs)))
 
-        return hits
-
     encoder.train()
     decoder.train()
 
+    return hits/len(pairs)
 
 
-def evaluateRandomly(encoder, decoder, n=10, verbose=False):
+
+
+def evaluateRandomly(encoder, decoder, device, n=10, verbose=False):
     encoder.eval()
     decoder.eval()
 
@@ -341,7 +344,7 @@ def evaluateRandomly(encoder, decoder, n=10, verbose=False):
             if verbose:
                 print('>', pair[0])
                 print('=', pair[1])
-            output_words = evaluate(encoder, decoder, pair[0])
+            output_words = evaluate(encoder, decoder, pair[0], device)
             output_sentence = ' '.join(output_words)
             if verbose:
                 print('<', output_sentence)
@@ -358,28 +361,49 @@ def evaluateRandomly(encoder, decoder, n=10, verbose=False):
     print('Hits {}/{} test samples'.format(hits, n))
 
 
-def saveModels(encoder, decoder, path):
-    torch.save({
-        'encoder_state_dict': encoder.state_dict(),
-        'decoder_state_dict': decoder.state_dict()
-        }, path)
-
+def saveModel(encoder, decoder, checkpoint  path):
+    checkpoint['encoder_state_dict'] = encoder.state_dict()
+    checkpoint['decoder_state_dict'] = decoder.state_dict()
+    torch.save(checkpoint, path)
 
 
 def loadParameters(encoder, decoder, path):
     checkpoint = torch.load(path)
     encoder.load_state_dict(checkpoint['encoder_state_dict'])
     decoder.load_state_dict(checkpoint['decoder_state_dict'])
+    return checkpoint
 
 
-encoder = EncoderRNN(INPUT_SIZE, HIDDEN_SIZE).to(device)
-decoder = DecoderRNN(HIDDEN_SIZE, OUTPUT_SIZE).to(device)
+def train_test_split(train_path, test_path, save_path):
+    train_path = 'scan/SCAN-master/' + train_path
+    test_path = 'scan/SCAN-master/' + test_path
+    _, _, train_pairs = prepareData('scan_in', 'scan_out', train_path, False)
+    _, _, test_pairs = prepareData('scan_in', 'scan_out', test_path, False)
 
-# trainIters(encoder, decoder, 5000)
-# evaluateTestSet(encoder, decoder, pairs)
-# saveModels(encoder, decoder, 'save_test.pt')
+    input_size = input_lang.n_words
+    hidden_size = 200
+    output_size = output_lang.n_words
 
-loadParameters(encoder, decoder, 'save_test.pt')
-evaluateTestSet(encoder, decoder, pairs)
-trainIters(encoder, decoder, 5000)
-evaluateTestSet(encoder, decoder, pairs)
+    encoder = EncoderRNN(input_size, hidden_size, device).to(device)
+    decoder = DecoderRNN(hidden_size, output_size, device).to(device)
+
+    train_losses = trainIters(encoder, decoder, 100000, device)
+    print('Evaluating training split accuracy')
+    train_acc = evaluateTestSet(encoder, decoder, train_pairs, device)
+    print('Evaluating test split accuracy')
+    test_acc = evaluateTestSet(encoder, decoder, test_pairs, device)
+
+    checkpoint = {'train_accuracy': train_acc,
+            'test_accuracy': test_acc,
+            'train_losses': train_losses}
+
+    saveModel(encoder, decoder, checkpoint, save_path)
+
+
+def loadModel(path):
+    encoder = EncoderRNN(input_size, hidden_size, device).to(device)
+    decoder = DecoderRNN(hidden_size, output_size, device).to(device)
+
+    checkpoint = loadParameters(encoder, decoder, path)
+    return encoder, decoder, checkpoint
+
