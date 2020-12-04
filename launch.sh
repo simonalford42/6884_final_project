786203135fe43fca4f202a5a72ae8ea3401462fe
Simon <salford@mit.edu>
2020-12-04 03:15:24 -0500

    cleaned up

--- launch.sh
+++ launch.sh
@@ -1,8 +1,6 @@
 # !/bin/sh
 
-python -u seq2seq.py --inter-rep2 --iters 10000 --model 'GRU' --gpu 4 --tag 5 --split 'mcd' >> out/mcd_gru_inter2.out &
-python -u seq2seq.py --iters 150000 --model 'GRU' --gpu 4 --tag 5 --split 'mcd' >> out/mcd_gru.out &
-python -u seq2seq.py --inter-rep --iters 150000 --model 'GRU' --gpu 4 --tag 5 --split 'mcd' >> out/mcd_gru_inter.out &
+python -u seq2seq.py --iters 150000 --model 'GRU' --gpu 4 --split 'mcd' >> out/mcd_gru.out &
 
 wait
 echo Group completed.
