7383219d3d7b90d5a6734da4cf49f21ecaa91b9e
Simon Alford <simonalford42@gmail.com>
2020-12-06 14:12:31 -0500

    some cleanup

--- launch.sh
+++ launch.sh
@@ -1,12 +1,8 @@
 # !/bin/sh
 
-<<<<<<< HEAD
 python -u seq2seq.py --iters 150000 --model 'GRU' --gpu 4 --split 'mcd' >> out/mcd_gru.out &
-
-=======
 python -u seq2seq.py --model 'GRU_A' --inter-rep --iters 15000 --split 'mcd' >> out/mcd_gru_a_inter.out &
 python -u seq2seq.py --model 'GRU_A' --iters 150000 --split 'mcd' >> out/mcd_gru_a.out &
->>>>>>> 7f9b1233f2e75954fc025586a676f815b77a5d34
 wait
 echo Group completed.
 
